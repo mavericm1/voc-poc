b28bdd965e64ed6c49628208d55aca0d8e819545
Logan Rawlins <maveric@gotskill.net>
2021-05-12 00:00:35 -0700

    fixing the node install

--- install-scripts/pi.sh
+++ install-scripts/pi.sh
@@ -14,7 +14,7 @@ git -C $installdir clone https://github.com/fpv-wtf/voc-poc.git
 
 sudo apt-get install -y libudev-dev
 
-npm install $targetdir
+npm install $targetdir --prefix $targetdir
 
 
 start
