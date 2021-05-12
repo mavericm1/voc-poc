d055913e5aa6216c9d87fd2054d9fcc21e22b3f1
Logan Rawlins <maveric@gotskill.net>
2021-05-11 23:56:31 -0700

    removing npm prefix

--- install-scripts/pi.sh
+++ install-scripts/pi.sh
@@ -14,9 +14,7 @@ git -C $installdir clone https://github.com/fpv-wtf/voc-poc.git
 
 sudo apt-get install -y libudev-dev
 
-mkdir $targetdir/node_modules
-
-npm install $targetdir --prefix $targetdir
+npm install $targetdir
 
 
 start
