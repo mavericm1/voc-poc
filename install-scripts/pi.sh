3cf32678dc4cc510f383ecc99d17c7c1afd28d04
Logan Rawlins <maveric@gotskill.net>
2021-05-12 00:08:49 -0700

    fixing npm install

--- install-scripts/pi.sh
+++ install-scripts/pi.sh
@@ -14,7 +14,7 @@ git -C $installdir clone https://github.com/fpv-wtf/voc-poc.git
 
 sudo apt-get install -y libudev-dev
 
-npm install --prefix $targetdir $targetdir 
+npm install --prefix $targetdir -g $targetdir 
 
 
 start
